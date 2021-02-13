c067e021962ee480f7243dae638023f592e94486
王一帆 <wang.yifan4@iwhalecloud.com>
2021-02-13 10:45:44 +0800

    - 单例模式

--- src/main/java/com/ivaneye/singleton/App.java
+++ src/main/java/com/ivaneye/singleton/App.java
@@ -3,29 +3,29 @@ package com.ivaneye.singleton;
 public class App {
 
   public static void main(String[] args) {
-    HungrySingleton ivoryTower1 = HungrySingleton.getInstance();
-    HungrySingleton ivoryTower2 = HungrySingleton.getInstance();
-    System.out.println("ivoryTower1={}" + ivoryTower1);
-    System.out.println("ivoryTower2={}" + ivoryTower2);
+    HungrySingleton hungrySingleton = HungrySingleton.getInstance();
+    HungrySingleton hungrySingleton1 = HungrySingleton.getInstance();
+    System.out.println("ivoryTower1={}" + hungrySingleton);
+    System.out.println("ivoryTower2={}" + hungrySingleton1);
 
-    LazySingleton threadSafeIvoryTower1 = LazySingleton.getInstance();
-    LazySingleton threadSafeIvoryTower2 = LazySingleton.getInstance();
-    System.out.println("threadSafeIvoryTower1={}" + threadSafeIvoryTower1);
-    System.out.println("threadSafeIvoryTower2={}" + threadSafeIvoryTower2);
+    LazySingleton lazySingleton = LazySingleton.getInstance();
+    LazySingleton lazySingleton1 = LazySingleton.getInstance();
+    System.out.println("threadSafeIvoryTower1={}" + lazySingleton);
+    System.out.println("threadSafeIvoryTower2={}" + lazySingleton1);
 
-    EnumSingleton enumIvoryTower1 = EnumSingleton.INSTANCE;
-    EnumSingleton enumIvoryTower2 = EnumSingleton.INSTANCE;
-    System.out.println("enumIvoryTower1={}" + enumIvoryTower1);
-    System.out.println("enumIvoryTower2={}" + enumIvoryTower2);
+    EnumSingleton enumSingleton = EnumSingleton.INSTANCE;
+    EnumSingleton enumSingleton1 = EnumSingleton.INSTANCE;
+    System.out.println("enumIvoryTower1={}" + enumSingleton);
+    System.out.println("enumIvoryTower2={}" + enumSingleton1);
 
-    DCLSingleton dcl1 = DCLSingleton.getInstance();
-    System.out.println(dcl1.toString());
-    DCLSingleton dcl2 = DCLSingleton.getInstance();
-    System.out.println(dcl2.toString());
+    DCLSingleton dclSingleton = DCLSingleton.getInstance();
+    System.out.println(dclSingleton.toString());
+    DCLSingleton dclSingleton1 = DCLSingleton.getInstance();
+    System.out.println(dclSingleton1.toString());
 
-    HolderSingleton demandHolderIdiom = HolderSingleton.getInstance();
-    System.out.println(demandHolderIdiom.toString());
-    HolderSingleton demandHolderIdiom2 = HolderSingleton.getInstance();
-    System.out.println(demandHolderIdiom2.toString());
+    HolderSingleton holderSingleton = HolderSingleton.getInstance();
+    System.out.println(holderSingleton.toString());
+    HolderSingleton holderSingleton1 = HolderSingleton.getInstance();
+    System.out.println(holderSingleton1.toString());
   }
 }
