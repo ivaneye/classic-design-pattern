d64266768fdf4123df22c9e2eb647a5a681703e2
王一帆 <wang.yifan4@iwhalecloud.com>
2021-02-20 11:00:59 +0800

    - 抽象工厂模式

--- src/main/java/com/ivaneye/abstractfactory/Main.java
+++ src/main/java/com/ivaneye/abstractfactory/Main.java
@@ -7,7 +7,11 @@ package com.ivaneye.abstractfactory;
 public class Main {
 
     public static void main(String[] args) {
+        // 根据环境设置工厂
         IOFactory ioFactory = new EpollFactory();
+//        IOFactory ioFactory = new SelectFactory();
+//        IOFactory ioFactory = new KqueueFactory();
+//        IOFactory ioFactory = new EvportFactory();
         Ae ae = new Ae(ioFactory);
         ae.aeMain();
     }
