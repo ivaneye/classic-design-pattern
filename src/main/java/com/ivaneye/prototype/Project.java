07caff87de86679d733b026df30eb2e2a74473e6
王一帆 <wang.yifan4@iwhalecloud.com>
2021-02-20 16:59:19 +0800

    - 原型模式

--- src/main/java/com/ivaneye/prototype/Project.java
+++ src/main/java/com/ivaneye/prototype/Project.java
@@ -6,8 +6,11 @@ package com.ivaneye.prototype;
  */
 public class Project implements Git {
 
+    // 项目名称
     private String name;
+    // 项目内容
     private String cont;
+    // 子项目
     private Git subTree;
 
     public Project(String name, String cont) {
